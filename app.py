46e4042b24e79e7a944629dad54ca78e0c98be71
andber6 <andre.bergan99@hotmail.com>
2020-05-14 14:04:35 +0000

    finally got mongodb to work correctly and made some progress

--- app.py
+++ app.py
@@ -1,22 +1,36 @@
 import os
+from flask_pymongo import PyMongo
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from os import path
+
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
-
 app.config["MONGO_DBNAME"] = 'Recipe_Database'
-app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
+app.config["MONGO_URI"]= os.environ.get('MONGO_URI')
+
 
 mongo = PyMongo(app)
 
 @app.route('/')
 @app.route('/get_recipes')
 def get_recipes():
-    return render_template("recipes.html", recipes=mongo.db.recipes.find())
+    _recipes = mongo.db.Creative_recipes.find()
+    recipe_list = [recipe for recipe in _recipes]
+    return render_template("recipes.html", 
+                            recipes = recipe_list)
+
+@app.route('/add_recipe')
+def add_task():
+    _categories = mongo.db.navigation.find()
+    category_list = [category for category in _categories]
+    return render_template('addrecipe.html',
+                           categories=mongo.db.navigation.find())
 
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
         port=os.environ.get('PORT'),
         debug=True)
-    
