a68f51042c715044c8d27da9f73107017bd17037
andber6 <andre.bergan99@hotmail.com>
2020-10-07 06:47:02 +0000

    adding more code as well as yml file and dockerfile etc

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_recipes')
 def get_recipes():
-    _recipes = mongo.db.Creative_recipes.find()
+    _recipes = mongo.db.recipes.find()
     recipe_list = [recipe for recipe in _recipes]
     return render_template('recipes.html', 
                             recipes = recipe_list)
@@ -32,10 +32,31 @@ def add_task():
 
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
-    recipes = mongo.db.Creative_recipes
+    recipes = mongo.db.recipes
     recipes.insert_one(request.form.to_dict())
     return redirect(url_for('get_recipes'))
 
+@app.route('/edit_recipe/<recipe_id>')
+def edit_recipe(recipe_id):
+    the_recipe =  mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
+    all_categories =  mongo.db.categories.find()
+    return render_template('editrecipe.html', recipe=the_recipe,
+                           categories=all_categories)
+
+@app.route('/update_recipe/<recipe_id>', methods=["POST"])
+def update_recipe(recipe_id):
+    recipes = mongo.db.recipes
+    recipes.update( {'_id': ObjectId(recipe_id)},
+    {
+        'recipe_name': request.form.get('recipe_name'),
+        'category_name': request.form.get('category_name'),
+        'recipe_description': request.form.get('recipe_description'),
+        'ingredients': request.form.get('ingredients'),
+        'isHealthy': request.form.get('isHealthy'),
+        'isQuickToMake': request.form.get('isQuickToMake'),
+        'submittedBy': request.form.get('submittedBy')
+    })
+    return render_template(url_for('get_recipes'))
 
 if __name__ == "__main__":
     # app.run(host=os.environ.get('IP'),
