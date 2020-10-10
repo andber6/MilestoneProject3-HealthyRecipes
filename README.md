a6cdcbe390f80d1d50016257d275e827b5e46355
andber6 <andre.bergan99@hotmail.com>
2020-10-10 10:26:52 +0000

    almost done with project

--- README.md
+++ README.md
@@ -1,16 +1,13 @@
 [![Gitpod ready-to-code](https://img.shields.io/badge/Gitpod-ready--to--code-blue?logo=gitpod)](https://gitpod.io/#https://github.com/andber6/MilestoneProject3-HealthyRecipes)
 
-         ___        ______     ____ _                 _  ___  
-        / \ \      / / ___|   / ___| | ___  _   _  __| |/ _ \ 
-       / _ \ \ /\ / /\___ \  | |   | |/ _ \| | | |/ _` | (_) |
-      / ___ \ V  V /  ___) | | |___| | (_) | |_| | (_| |\__, |
-     /_/   \_\_/\_/  |____/   \____|_|\___/ \__,_|\__,_|  /_/ 
- ----------------------------------------------------------------- 
+# My Public Recipe List! #
+
+In this project I am creating a python project with flask and using mongoDB as my database to store and update records/recipes.
+The goal of this project is simply to get a better understanding of the technologies used (refer to 'Techonologies used' section further down).
+
+## UX ##
+
+In this recipe project I have spent a lot of time getting it all to work correctly.
+I have met many challenges along the way when routing and updating new records, but I feel much more confident and I'm pleased with the result.
 
 
-Hi there! Welcome to AWS Cloud9!
-
-To get started, create some files, play with the terminal,
-or visit https://docs.aws.amazon.com/console/cloud9/ for our documentation.
-
-Happy coding!
--- app.py
+++ app.py
@@ -18,45 +18,66 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_recipes')
 def get_recipes():
+    all_categories =  mongo.db.categories.find()
     _recipes = mongo.db.recipes.find()
     recipe_list = [recipe for recipe in _recipes]
-    return render_template('recipes.html', 
+    return render_template('recipes.html',
+                            categories=all_categories,
                             recipes = recipe_list)
 
 @app.route('/add_recipe')
-def add_task():
-    _categories = mongo.db.navigation.find()
-    category_list = [category for category in _categories]
+def add_recipe():
+    all_categories =  mongo.db.categories.find()
     return render_template('addrecipe.html',
-                           categories=mongo.db.navigation.find())
+                           categories=all_categories,
+                           recipes=mongo.db.recipes.find())
 
 @app.route('/insert_recipe', methods=['POST'])
 def insert_recipe():
     recipes = mongo.db.recipes
     recipes.insert_one(request.form.to_dict())
-    return redirect(url_for('get_recipes'))
+    return render_template('recipes.html',
+                            categories=mongo.db.categories.find(),
+                            recipes=mongo.db.recipes.find())
 
 @app.route('/edit_recipe/<recipe_id>')
 def edit_recipe(recipe_id):
     the_recipe =  mongo.db.recipes.find_one({"_id": ObjectId(recipe_id)})
     all_categories =  mongo.db.categories.find()
-    return render_template('editrecipe.html', recipe=the_recipe,
+    return render_template('editrecipe.html', 
+                           recipe=the_recipe,
                            categories=all_categories)
 
 @app.route('/update_recipe/<recipe_id>', methods=["POST"])
 def update_recipe(recipe_id):
     recipes = mongo.db.recipes
-    recipes.update( {'_id': ObjectId(recipe_id)},
+    all_categories =  mongo.db.categories.find()
+    recipes.update({"_id": ObjectId(recipe_id)},
     {
-        'recipe_name': request.form.get('recipe_name'),
         'category_name': request.form.get('category_name'),
+        'recipe_name': request.form.get('recipe_name'),
         'recipe_description': request.form.get('recipe_description'),
         'ingredients': request.form.get('ingredients'),
         'isHealthy': request.form.get('isHealthy'),
         'isQuickToMake': request.form.get('isQuickToMake'),
         'submittedBy': request.form.get('submittedBy')
     })
-    return render_template(url_for('get_recipes'))
+    return render_template('recipes.html',
+                            categories=all_categories,
+                            recipes = mongo.db.recipes.find())
+
+@app.route('/delete_recipe/<recipe_id>')
+def delete_recipe(recipe_id):
+    all_categories =  mongo.db.categories.find()
+    mongo.db.recipes.remove({'_id': ObjectId(recipe_id)})
+    return render_template('recipes.html',
+                            categories = all_categories,
+                            recipes=mongo.db.recipes.find())
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 
 if __name__ == "__main__":
     # app.run(host=os.environ.get('IP'),
